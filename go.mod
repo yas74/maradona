dc6420569b63a85fa951d531968d332c2cc79e8c
Yasaman <yas.gr60@gmail.com>
2026-05-14 10:35:07 +0330

    add health-check endpoint

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/yas74/maradona
+
+go 1.25.0
+
+require github.com/labstack/echo v3.3.10+incompatible
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/gommon v0.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.21 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.51.0 // indirect
+	golang.org/x/net v0.53.0 // indirect
+	golang.org/x/sys v0.44.0 // indirect
+	golang.org/x/text v0.37.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	router := echo.New()
+
+	router.Use(middleware.Logger())
+
+	router.GET("/health-check", healthCheck)
+
+	port := getEnv("MARADONA_HTTP_PORT", "8080")
+
+	if err := router.Start(fmt.Sprintf(":%s", port)); err != nil {
+		panic(err)
+	}
+}
+
+func healthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, echo.Map{
+		"message": "everything is good",
+	})
+}
+
+func getEnv(k, d string) string {
+	if v := os.Getenv(k); v != "" {
+		return v
+	}
+
+	return d
+}
